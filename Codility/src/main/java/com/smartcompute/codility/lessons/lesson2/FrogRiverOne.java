c04a2886f96dbb047145ab002c9f31be52822002
hasanmasud <masudbook@gmail.com>
2015-09-20 19:44:28 +0200

    commit

---
+++ Codility/src/main/java/com/smartcompute/codility/lessons/lesson2/FrogRiverOne.java
@@ -0,0 +1,12 @@
+package com.smartcompute.codility.lessons.lesson2;
+
+public class FrogRiverOne {
+	
+	 public int solution(int X, int[] A) {
+	        // write your code in Java SE 8
+		 
+		 
+		 return 1;
+		 
+	    }
+}
