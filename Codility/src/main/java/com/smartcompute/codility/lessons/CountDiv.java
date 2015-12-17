1e467581d76f33ca165fde2a6832b06ddfa59148
hasanmasud <masudbook@gmail.com>
2015-12-17 21:43:35 +0100

    commit

---
+++ Codility/src/main/java/com/smartcompute/codility/lessons/CountDiv.java
@@ -0,0 +1,13 @@
+package com.smartcompute.codility.lessons;
+
+public class CountDiv {
+
+	public int solution(int A, int B, int K) {
+		int offsetForLeftRange = 0;
+		if (A % K == 0) {
+			++offsetForLeftRange;
+		}
+		return (B / K) - (A / K) + offsetForLeftRange;
+	}
+
+}
