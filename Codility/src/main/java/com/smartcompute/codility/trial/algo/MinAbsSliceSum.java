5aa007ff9a0680c8892ad2799c21beaf4b7efb8a
hasanmasud <masudbook@gmail.com>
2015-12-22 18:19:58 +0100

    commit

--- Codility/src/main/java/com/smartcompute/codility/trial/algo/MinAbsSliceSum.java
+++ Codility/src/main/java/com/smartcompute/codility/trial/algo/MinAbsSliceSum.java
@@ -1,65 +1,50 @@
 package com.smartcompute.codility.trial.algo;
 
+import java.util.Arrays;
 
 // need to write it correctly
 
 public class MinAbsSliceSum {
-	public static int solution(int[] A) {
-		if(A.length < 1 ){
-			return 0;
-		}
-		if(A.length == 1){
+
+	public int solution(int[] A) {
+
+		if (A.length == 1) {
 			return Math.abs(A[0]);
 		}
-		long min = Long.MAX_VALUE;
+		int min = Integer.MAX_VALUE;
 		
-		for(int i = 0 ; i < A.length - 1 ; i++){
-			long temp = A[i];
-			for(int j = i+1 ; j < A.length; j++){
+		for(int i = 0 ; i < A.length -1 ; i++){
+			int temp = A[i];
+			
+			for(int j = i+1 ; j < A.length ; j++){
 				temp  += A[j];
-				if(min > Math.abs(temp)){
-					min = Math.abs(temp);
-				}
+				min = Math.min(min, Math.abs(temp));
 				if(min == 0){
 					return 0;
 				}
 			}
+			
+			
 		}
-		return (int)min;
+		return min;
+
 	}
-	
-	
-	public static void main(String ...args){
-		
-		
-		int a[] = {2,-4,6,-3,9};
-		System.out.println(solution(a));
-		
-		
-		int b[] = {-1,1,3,4,5};
-		System.out.println(solution(b));
-		
-		int c[] = {5,5,5,5,5};
-		System.out.println(solution(c));
-		
-		
-		int d[] = {-5,-5,-5,-5,-5};
-		System.out.println(solution(d));
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+	public static void main(String... args) {
+
+		MinAbsSliceSum obj = new MinAbsSliceSum();
+
+		int a[] = { 2, -4, 6, -3, 9 };
+		System.out.println(obj.solution(a));
+
+		int b[] = { -1, 1, 3, 4, 5 };
+		System.out.println(obj.solution(b));
+
+		int c[] = { 5, 5, 5, 5, 5 };
+		System.out.println(obj.solution(c));
+
+		int d[] = { -5, -5, -5, -5, -5 };
+		System.out.println(obj.solution(d));
+
 	}
 }
