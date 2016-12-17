7143cf88fb6041ed3afa144790ebbabdedfdde7f
hasanmasud <masudbook@gmail.com>
2016-12-17 23:51:22 +0530

    commit

--- Codility/src/main/java/com/smartcompute/codility/lessons/lesson1/Test.java
+++ Codility/src/main/java/com/smartcompute/codility/lessons/lesson1/Test.java
@@ -1,13 +1,13 @@
 package com.smartcompute.codility.lessons.lesson1;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Test {
-	
-	public static void main(String ...args){
-		
-		
+	public static void main(String... args) throws IOException {
 		double x = 999999999999999d;
 		System.out.println(x);
-		
+		InputStream source = Runtime.getRuntime().exec("dir").getInputStream();
+		System.out.println(source.toString());
 	}
-
 }
