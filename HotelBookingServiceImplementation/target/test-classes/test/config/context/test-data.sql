8ff33ce2fb833e15404dfffe25ea8d5063c9d443
hasanmasud <masudbook@gmail.com>
2016-12-22 22:36:44 +0530

    commit

---
+++ HotelBookingServiceImplementation/target/test-classes/test/config/context/test-data.sql
@@ -0,0 +1,10 @@
+insert into ROOM values(1,100,'AVAILABLE',12000);
+insert into ROOM values(2,125,'AVAILABLE',2000);
+insert into ROOM values(3,126,'AVAILABLE',50000);
+
+
+insert into ROOM values(4,113,'BOOKED',12000);
+insert into ROOM values(5,150,'BOOKED',12000);
+insert into ROOM values(6,100,'BOOKED',12000);
+insert into ROOM values(7,250,'BOOKED',70000);
+commit;
